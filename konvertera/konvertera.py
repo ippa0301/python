59d537b25c114edffd8f9bf8a210e4a6a7f72c32
Filippa Erlandsdotter <fier0301@storsjogymnasiet.se>
2023-12-08 13:15:32 +0100

    si

---
+++ konvertera/konvertera.py
@@ -0,0 +1,7 @@
+def konvertera_ord (input_string):
+    converted=input_string[0] + input_string[1].lower()
+    return converted
+
+word = input("Skriv in ett ord du vill konvertera:  ")
+konverterat_ord = konvertera_ord(word)
+print(konverterat_ord)
